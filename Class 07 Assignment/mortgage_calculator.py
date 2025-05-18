8a5270dbdb4453e6b3ac14a10cbd7307c27fb4bf
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-05-18 10:44:45 +0500

    Update mortgage_calculator.py

--- Class 07 Assignment/mortgage_calculator.py
+++ Class 07 Assignment/mortgage_calculator.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import math
 
 st.title("Mortgage Repayments Calculator")
@@ -12,7 +11,7 @@ deposit = col1.number_input("Deposit", min_value=0, value=100000)
 interest_rate = col2.number_input("Interest Rate (in %)", min_value=0.0, value=5.5)
 loan_term = col2.number_input("Loan Term (in years)", min_value=1, value=30)
 
-# Calculate the repayments.
+# Calculate the repayments
 loan_amount = home_value - deposit
 monthly_interest_rate = (interest_rate / 100) / 12
 number_of_payments = loan_term * 12
@@ -22,7 +21,7 @@ monthly_payment = (
     / ((1 + monthly_interest_rate) ** number_of_payments - 1)
 )
 
-# Display the repayments.
+# Display the repayments
 total_payments = monthly_payment * number_of_payments
 total_interest = total_payments - loan_amount
 
@@ -32,8 +31,7 @@ col1.metric(label="Monthly Repayments", value=f"${monthly_payment:,.2f}")
 col2.metric(label="Total Repayments", value=f"${total_payments:,.0f}")
 col3.metric(label="Total Interest", value=f"${total_interest:,.0f}")
 
-
-# Create a data-frame with the payment schedule.
+# Create payment schedule
 schedule = []
 remaining_balance = loan_amount
 
@@ -41,24 +39,22 @@ for i in range(1, number_of_payments + 1):
     interest_payment = remaining_balance * monthly_interest_rate
     principal_payment = monthly_payment - interest_payment
     remaining_balance -= principal_payment
-    year = math.ceil(i / 12)  # Calculate the year into the loan
-    schedule.append(
-        [
-            i,
-            monthly_payment,
-            principal_payment,
-            interest_payment,
-            remaining_balance,
-            year,
-        ]
-    )
+    year = math.ceil(i / 12)
+    schedule.append([
+        i,
+        monthly_payment,
+        principal_payment,
+        interest_payment,
+        remaining_balance,
+        year,
+    ])
 
 df = pd.DataFrame(
     schedule,
     columns=["Month", "Payment", "Principal", "Interest", "Remaining Balance", "Year"],
 )
 
-# Display the data-frame as a chart.
+# Display the chart
 st.write("### Payment Schedule")
 payments_df = df[["Year", "Remaining Balance"]].groupby("Year").min()
 st.line_chart(payments_df)
